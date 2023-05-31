262afa7df3967f3104f2f1c33d78f83ff9115578
pamiii12 <fahmiadityaa03@gmail.com>
2023-05-31 16:44:12 +0700

    final MultiWAris

---
+++ MultiWaris/MultiWaris.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(stringg pNama) :
+		nama(pNama)
+	{
+		cout << "orang dibuuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
+
+class manusia {
+public:
+	string jenisKelamin;
+
+	manusia(string pJenisKelamin) :
+		jenisKelamin(pJenisKelamin)
+	{
+		cout << "manusia dibuat\n" << endl;
+	}
+	~manusia() {
+		cout << "manusia dihapus\n" << endl;
+	}
+};
+
+class pelajar : private manusia, public orang {
+public :
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) 
+	{
+		cout << " pelajar dibuat\n" << endl;
+	}
+	~pelajar()
+	{
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dengan jenis kelamin " + jenisKelamin + " dari sekolah " + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar siswa("andi laksono", "Laki-Laki", "SMAN 1 Bantul");
+	cout << siswa.perkenalan();
+
+	return 0;
+}
